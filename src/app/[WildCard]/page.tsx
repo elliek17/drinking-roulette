32606fc09fed188f9e2756e8675b331fbf8f4cb7
Elina <elliekarout@gmail.com>
2025-02-21 13:55:36 +0200

    Revert "trial for dynamics"
    This reverts commit 5bb846d6b24d48ba1bd9184666bc5229a6f2074e.

--- src/app/[WildCard]/page.tsx
+++ src/app/[WildCard]/page.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/button"
 import { promises as fs } from 'fs';
 
 export default async function Home() {
-  const file = await fs.readFile(process.cwd() + '/questions.json', 'utf8');
+  const file = await fs.readFile(process.cwd() + '/src/data/questions.json', 'utf8');
   const data = JSON.parse(file);
   const index: number = Math.floor(Math.random()*9);
   return (
