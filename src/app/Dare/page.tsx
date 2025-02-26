635ffc8289e68f69f0dd2a7c410242c1c8985be4
Elina <elliekarout@gmail.com>
2025-02-26 02:50:11 +0200

    data change

--- src/app/Dare/page.tsx
+++ src/app/Dare/page.tsx
@@ -3,12 +3,13 @@ import Button from "@/components/button"
 import { promises as fs } from 'fs';
 
 export default async function Home() {
-  const file = await fs.readFile(process.cwd() + '/src/data/questions.json', 'utf8');
+  const file = await fs.readFile(process.cwd() + '/src/data/dare.json', 'utf8');
   const data = JSON.parse(file);
+  const index: number = Math.floor(Math.random()*9);
   return (
     <div>
       <div className="grid grid-row-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Card category='Dare' question={data.Dare[Math.floor(Math.random()*9)]} />
+      <Card category='Dare' question={data.questions[index]} />
       <Button />
       </div>
     </div>
--- src/app/Truth/page.tsx
+++ src/app/Truth/page.tsx
@@ -3,13 +3,13 @@ import Button from "@/components/button"
 import { promises as fs } from 'fs';
 
 export default async function Home() {
-  const file = await fs.readFile(process.cwd() + '/src/data/questions.json', 'utf8');
+  const file = await fs.readFile(process.cwd() + '/src/data/truth.json', 'utf8');
   const data = JSON.parse(file);
   const index: number = Math.floor(Math.random()*9);
   return (
     <div>
       <div className="grid grid-row-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Card category='Truth' question={data.Truth[index]} />
+      <Card category='Truth' question={data.questions[index]} />
       <Button />
       </div>
     </div>
